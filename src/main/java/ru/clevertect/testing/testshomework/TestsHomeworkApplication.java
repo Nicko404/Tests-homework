da3b7f375b44a17e62fded2dc1185cb8821d565b
Nicko404 <kolsak2013@yandex.ru>
2024-09-09 22:10:34 +0300

    Initial commit

---
+++ src/main/java/ru/clevertect/testing/testshomework/TestsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertect.testing.testshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsHomeworkApplication.class, args);
+    }
+
+}
